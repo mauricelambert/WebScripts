5006fed253c2a39888bfee12dc03a90e0e15b473
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2021-12-24 13:05:57 +0100

    [2.4.6] More logs and rewrite code WebScripts.py
     - Fix: float in JSON WebScripts request
     - Fix: 404 on the end of process with real time output
     - Fix: documentation not found (permissions on directory)
     - Fix: multiple hardening rule on the same directory
     - Fix: blacklist is not working
     - Check HTTP method (and error 400)
     - Add logs in WebScripts.py
     - Add a token for API
     - Add log attribute on server instance

--- WebScripts/scripts/py/test_config.py
+++ WebScripts/scripts/py/test_config.py
@@ -1,6 +1,7 @@
+from sys import argv, stdin, stderr
 from functools import partial
-from sys import argv, stdin
 from time import sleep
+from os import environ
 
 print = partial(print, flush=True)
 
@@ -10,12 +11,16 @@ print("Inputs:")
 print(inputs)
 print("Inputs end.")
 
+print(f"Log path: {environ['LOG_PATH']}")
+
 sleep(5)
 print("5 seconds...")
 sleep(5)
 print("10 seconds...")
 sleep(5)
 print("15 seconds...")
+stderr.write("My custom error (15 seconds)...")
+stderr.flush()
 
 if "-t" in argv or "--timeout" in argv:
     print("TimeoutError is comming... (please wait)...")
