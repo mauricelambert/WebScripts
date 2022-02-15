93988faccadae91fe9e19f1d1945abc7ce3a0daf
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2022-02-15 18:51:43 +0100

    [2.5.4] Fix: FileNotFound logs

--- WebScripts/scripts/py/show_license.py
+++ WebScripts/scripts/py/show_license.py
@@ -3,7 +3,7 @@
 
 ###################
 #    This file may display the license and copyright of WebScripts
-#    Copyright (C) 2021  Maurice Lambert
+#    Copyright (C) 2021, 2022  Maurice Lambert
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -19,11 +19,13 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ###################
 
-"""This tool run scripts and display the result in a Web Interface.
+"""
+This tool run scripts and display the result in a Web Interface.
 
-This file may display the license and copyright of WebScripts."""
+This file may display the license and copyright of WebScripts.
+"""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Maurice Lambert"
 __author_email__ = "mauricelambert434@gmail.com"
 __maintainer__ = "Maurice Lambert"
@@ -35,7 +37,7 @@ __license__ = "GPL-3.0 License"
 __url__ = "https://github.com/mauricelambert/WebScripts"
 
 copyright = """
-WebScripts  Copyright (C) 2021  Maurice Lambert
+WebScripts  Copyright (C) 2021, 2022  Maurice Lambert
 This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions.
@@ -45,33 +47,41 @@ __copyright__ = copyright
 
 __all__ = []
 
-from os import path, chdir
-import sys
+from sys import exit, argv, stderr
+from os.path import dirname, join
+from os import chdir
 
 
-def main() -> None:
+def main() -> int:
 
-    """Main function to display license and copyright of WebScripts."""
+    """
+    Main function to display license and copyright of WebScripts.
+    """
 
-    chdir(path.join(path.dirname(__file__), "..", ".."))
+    chdir(join(dirname(__file__), "..", ".."))
 
-    if len(sys.argv) < 2:
-        print("USAGE: show_license.py [part required string]")
-        sys.exit(1)
+    if len(argv) < 2:
+        print(
+            "USAGE: show_license.py [part required string]",
+            file=stderr,
+        )
+        return 1
 
-    if "copyright" in sys.argv:
-        sys.argv.remove("copyright")
+    remove = argv.remove
+
+    if "copyright" in argv:
+        remove("copyright")
         print(copyright)
 
-    if "license" in sys.argv:
-        sys.argv.remove("license")
+    if "license" in argv:
+        remove("license")
         with open("LICENSE.txt") as license:
             print(license.read())
 
-    if "codeheader" in sys.argv:
-        sys.argv.remove("codeheader")
+    if "codeheader" in argv:
+        remove("codeheader")
         print(
-            """    Copyright (C) 2021  Maurice Lambert
+            """    Copyright (C) 2021, 2022  Maurice Lambert
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -84,13 +94,19 @@ def main() -> None:
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+            """
         )
 
-    if len(sys.argv) > 1:
-        print(f"ERROR: unexpected arguments {sys.argv[1:]}")
+    if len(argv) > 1:
+        print(
+            f"ERROR: unexpected arguments {argv[1:]}",
+            file=stderr,
+        )
+        return 2
+
+    return 0
 
 
 if __name__ == "__main__":
-    main()
-    sys.exit(0)
+    exit(main())
