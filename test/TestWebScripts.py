9c7fe914370a59e4bc441f23d19a1bb7344c8e10
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2022-07-13 08:44:35 +0200

    Update TestWebScripts.py

--- test/TestWebScripts.py
+++ test/TestWebScripts.py
@@ -95,8 +95,10 @@ try:
     WebScripts.WebScripts
 except AttributeError:
     target = "WebScripts.check_file_permission"
+    WebScripts.check_file_permission = lamda *x, **y: True
 else:
     target = "WebScripts.WebScripts.check_file_permission"
+    WebScripts.WebScripts.check_file_permission = lamda *x, **y: True
 
 
 class TestConfiguration(TestCase):
