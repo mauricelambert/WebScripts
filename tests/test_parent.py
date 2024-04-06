128449c9ca97c097b9860598879bff6f7d46395f
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2024-04-06 20:00:09 +0000

    Delete tests directory

--- tests/test_parent.py
+++
@@ -1,8 +0,0 @@
-from os import system, environ
-from threading import Thread
-
-environ["Test"] = "Yes test is good !"
-print(f"test_parent.py: {environ.get('Test')}")
-Thread(target=system, args=("test_enfant.py",)).start()
-environ["Test"] = "No is not good !"
-system("test_enfant2.py")
